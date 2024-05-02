e967b9226add80416cc10bc435f69a4bd761869a
ishpreet-kaur-bedi <kaur.ishpreet.bedi@gmail.com>
2024-05-03 00:16:49 +0530

    Sidebar Added

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,24 +1,27 @@
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { FeedWrapper } from "@/components/feed-wrapper";
+import { UserProgress } from "@/components/user-progress";
+
 import { Header } from "./header";
+import { title } from "process";
 
 const LearnPage = () => {
-    return(
-        <div className="flex flex-row-reverse gap-[48px] px-6">
-            <StickyWrapper>
-                {/* <UserProgress 
-                activeCourse={{}}
-                hearts={5}
-                points={100}
-                hasActiveSubscription={false}
-                /> */}
-            </StickyWrapper>
-
-            <FeedWrapper>
-                <Header title="Spanish"/>
-            </FeedWrapper>
-        </div>
-    );
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      {/* name of the course user will be taking  */}
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -13,8 +13,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="max-w-[998px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
       <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        {/* heroimage */}
         <Image src="/hero.svg" fill alt="Hero"></Image>
       </div>
 
