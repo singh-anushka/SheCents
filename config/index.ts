4b3907427a64e7d3c8dd757dd7ed6de2750ab57a
ishpreet-kaur-bedi <kaur.ishpreet.bedi@gmail.com>
2024-05-03 21:36:11 +0530

    bugs fixed

---
+++ config/index.ts
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "shecents",
+  description:
+    "Interactive financial app for women",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "duolingo-clone",
+    "learn-language",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "lingo",
+    "postgresql",
+    "sonner",
+    "drizzle",
+    "zustand",
+    "mysql",
+    "lucide-react",
+    "next-themes",
+    "clerk-themes",
+    "clerk",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Anushka Singh, Ishpreet Kaur , Siddhi Aggarwal",
+    url: "https://github.com/singh-anushka/SheCents",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/singh-anushka/SheCents",
+} as const;
